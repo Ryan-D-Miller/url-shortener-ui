59352500883031cba1b2ef9871c89a4a231d6151
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-15 10:03:43 -0600

    Test the app and that the title and Url cards are there

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -3,7 +3,10 @@ describe('App', () => {
         cy.loadStubs();
         cy.visit('http://localhost:3000/')
     })
-    it('should tesst that everything is working', () => {
-
+    it('Should have a title', () => {
+        cy.get('h1').should('have.text', 'URL Shortener')
+    })
+    it('Should have a shortened urlContainer', () => {
+        cy.get('.url').first().children().eq(0).should('have.text', 'Awesome photo')
     })
 })
---
+++ cypress/integration/Form.spec.js
@@ -0,0 +1,7 @@
+describe('Form', () => {
+    beforeEach(() => {
+        cy.loadStubs();
+        cy.visit('http://localhost:3000/')
+    })
+    it('Should bee able to select and ')
+})
