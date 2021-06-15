ddd64ac94ad26dba2d0126de8b642912780d5ac1
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-15 11:00:44 -0600

    Add testing for an errored response from get request

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,12 +1,20 @@
 describe('App', () => {
     beforeEach(() => {
-        cy.loadStubs();
-        cy.visit('http://localhost:3000/')
+
     })
     it('Should have a title', () => {
+        cy.loadStubs();
+        cy.visit('http://localhost:3000/')
         cy.get('h1').should('have.text', 'URL Shortener')
     })
     it('Should have a shortened urlContainer', () => {
-        cy.get('.url').first().children().eq(0).should('have.text', 'Awesome photo')
+        cy.loadStubs();
+        cy.visit('http://localhost:3000/')
+        cy.get('.url').first().children().eq(0).should('have.text', 'Stubbed data')
     })
+    it('should say something went wrong if an error is returned on get request', () => {
+        cy.intercept('http://localhost:3001/api/v1/urls', {statusCode: 404, body: "cypress forced error"})
+        cy.visit('http://localhost:3000/')
+        cy.get('main').children().eq(1).should('have.text', "Something went Wrong")
+    })    
 })
