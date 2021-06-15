c28f5a547433fe2fc2aacae3a7a82ec72c6c20cd
Ryan Miller <ryandmiller3521@gmail.com>
2021-06-15 10:10:43 -0600

    Add form checking for input fields

--- cypress/integration/Form.spec.js
+++ cypress/integration/Form.spec.js
@@ -3,5 +3,22 @@ describe('Form', () => {
         cy.loadStubs();
         cy.visit('http://localhost:3000/')
     })
-    it('Should bee able to select and ')
+    it('Should bee able to select and fill out the form and see the updated information', () => {
+        cy.get('input[name="title"]')
+            .type("newUrl")
+            .should('have.value', 'newUrl')
+        cy.get('input[name="long_url"]')
+            .type("http://ImanewLongURL.com/withtomuchinformation")
+            .should('have.value', "http://ImanewLongURL.com/withtomuchinformation")
+    })
+    it('Should be able to submit information to the form and see see that the fields are emptied', () => {
+        cy.get('input[name="title"]')
+            .type("newUrl")
+        cy.get('input[name="long_url"]')
+            .type("http://ImanewLongURL.com/withtomuchinformation")
+        cy.get('button').click()
+        cy.get('input[name="title"]').should('have.value', '')
+        cy.get('input[name="long_url"]').should('have.value', '')
+    })
+    it('Should')
 })
